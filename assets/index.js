788e55195ef3e32cf8c25c7cbd5b4dfdfba48b27
Reynaldo <reynaldomoreno173@gmail.com>
2021-07-24 15:46:51 -0400

    Creacion de ServicesWorker

--- assets/index.js
+++ assets/index.js
@@ -14,3 +14,9 @@ const button1 = document.getElementById("mute")
 
 button.onclick = () => player.togglePlay();
 button1.onclick = () => player.toggleMute();
+
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.register('/sw.js').catch(error => {
+    console.log(error.message);
+  })
+}
