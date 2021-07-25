9f319296f883f7e19a5e850b533628561f3a6775
Reynaldo <reynaldomoreno173@gmail.com>
2021-07-25 10:49:05 -0400

    Practicando Ts

--- sw.js
+++ sw.js
@@ -12,42 +12,34 @@ self.addEventListener('fetch', event => {
   }
 
   //Buscar en cache
-  event.respondWith(cacheResponse(request))
+  event.respondWith(cachedResponse(request))
 
   //Actualizar el cache
   event.waitUntil(updateCache(request))
 })
 
-const precache = async () => {
-  try {
-    const cache = await caches.open(version);
-    return cache.addAll([
-      '/',
-      './index.html',
-      './assets/index.js',
-      './assets/MediaPlayer.js',
-      './assets/plugins/AutoPlay.js',
-      './assets/plugins/AutoPause.js',
-      './assets/index.css',
-      './assets/BigBuckBunny.mp4',
-    ])
-  } catch (error) {
-    console.log(error.message);
-  }
+async function precache() {
+  const cache = await caches.open(version);
+  return cache.addAll([
+    './',
+    './index.html',
+    './assets/index.js',
+    './assets/MediaPlayer.js',
+    './assets/plugins/AutoPlay.js',
+    './assets/plugins/AutoPause.js',
+    './assets/index.css',
+    './assets/BigBuckBunny.mp4',
+  ]);
 }
 
-const cacheResponse = async (request) => {
-  try {
-    const cache = await caches.open(version);
-    const response = await cache.match(request)
-    return response || fetch(request);
-  } catch (error) {
-    console.log(error.message);
-  }
+async function cachedResponse(request) {
+  const cache = await caches.open(version);
+  const response = await cache.match(request)
+  return response || fetch(request);
 }
 
 //Con esta funcion nos aseguramos de siempre devolver algo, si no esta en cache hacemos la peticion a fetch.
-const updateCache = async (request) => {
+async function updateCache(request) {
   const cache = await caches.open(version)
   const response = await fetch(request)
   return response.status === 200 ? cache.put(request, response) : new Promise((resolve, reject) => resolve(':)'))
